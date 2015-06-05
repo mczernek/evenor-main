476017718780ac0a56f1861dbd5b559bf11cdc6d
Łukasz Patecki <patman@ukaszs-MacBook-Pro.local>
2015-06-06 00:06:26 +0200

    :basic presentation of events

--- engine/database/views.py
+++ engine/database/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader, RequestContext
+from database.models import Event, Comment
 
-# Create your views here.
+
+def index(request):
+    event_list = Event.objects.order_by('event_name')[:5]
+    template = loader.get_template('database/events_list.html')
+    context = RequestContext(request, {
+        'event_list': event_list,
+    })
+    return HttpResponse(template.render(context))
+
+
+def event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    comment_list = Comment.objects.filter(event_id = event_id).select_related('user')
+    context = {'event': event,
+               'comment_list': comment_list}
+    return render(request, 'database/event_detail.html', context)
--- engine/engine/urls.py
+++ engine/engine/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from database import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<event_id>[0-9]+)/$', views.event, name='event'),
 ]
